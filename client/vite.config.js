3ccc3e300f945e453d85dfb676e2483a0ec7ae8a
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2025-07-16 18:01:32 +0530

    fix: Fixed minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // ✅ ensures assets are resolved from root
+  base: "./", // ✅ ensures assets are resolved from root
   plugins: [react()],
 });
